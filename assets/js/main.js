6a570e472be12834699f3074d33fc57b59634bb5
ismailkrc57 <ismailkrc57@gmail.com>
2021-10-27 21:40:58 +0300

    digital clock is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
 const hour = document.getElementById("clock-hour"),
   minutes = document.getElementById("clock-minutes"),
   seconds = document.getElementById("clock-seconds");
+const mysaat = document.getElementById("mysaat"),
+  mydakika = document.getElementById("mydakika"),
+  mysaniye = document.getElementById("mysaniye");
 
 const clock = () => {
   let date = new Date();
   let hh = date.getHours() * 30,
     mm = date.getMinutes() * 6,
     ss = date.getSeconds() * 6;
-    console.log(hh,mm,ss)
-    hour.style.transform=`rotateZ(${hh+mm/12}deg)`
-    minutes.style.transform=`rotateZ(${mm}deg)`
-    seconds.style.transform=`rotateZ(${ss}deg)`
+  console.log(hh, mm, ss);
+  hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  minutes.style.transform = `rotateZ(${mm}deg)`;
+  seconds.style.transform = `rotateZ(${ss}deg)`;
+  mysaat.innerText = `${date.getHours()}`;
+  mydakika.innerText = `${date.getMinutes()}`;
+  mysaniye.innerText = `${date.getSeconds()}`;
 };
 
-setInterval(clock,1000)
+setInterval(clock, 1000);
